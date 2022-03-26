59c118ad3836c8ea60889e2c00dd6c2adb662a5a
theo.stosse <theo.stosse@epita.fr>
2022-03-26 04:19:24 +0000

    blob part1

--- python/interpret-blob/blob.py
+++ python/interpret-blob/blob.py
@@ -1,6 +1,23 @@
 import csv
 
-
 def read_dictionary(file_name):
-    f = open("dictionary/" + file_name)
-    dico = f.read('\n').split()
+    f = open("/home/theos/french_ica/python/interpret-blob/dictionary/" + file_name)
+    dico = f.read().split('\n')
+    return dico
+
+def read_profane_to_common(file_name):
+    f = open("/home/theos/french_ica/python/interpret-blob/dictionary/" + file_name)
+    tmp = f.read().split('\n')
+
+    dico = {}
+    for e in tmp:
+        e2 = e.split(";")
+        dico.update({e2[0] : e2[1]})
+    return dico
+
+def pro_to_com(str, dico):
+    for key in dico:
+        s = key.lower()
+        print(key)
+        str = str.replace(s, dico[key], 1)
+    return str
