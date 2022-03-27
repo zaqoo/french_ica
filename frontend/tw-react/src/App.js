604208991e04278112c80c50e243ce65245c9270
theo.stosse <theo.stosse@epita.fr>
2022-03-27 13:44:43 +0100

    fdf

--- frontend/tw-react/src/App.js
+++ frontend/tw-react/src/App.js
@@ -103,7 +103,7 @@ function Example(props) {
     {MedicamentToTable(props.ordonnance.medicaments)}
   </div>
   <div>
-  <a className="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" href="ordonnance.pdf" rel="noopener noreffer" target="_blank">Valider</a>
+  <a className="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" href="ordoPharma.pdf ordoLabo.pdf" rel="noopener noreffer" target="_blank">Valider</a>
   </div>
 </form>
   )
